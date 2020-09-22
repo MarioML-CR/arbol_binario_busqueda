088dd8d10593c1e4318cf6238f4d0db0055bf195
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2020-09-22 11:57:04 -0600

    se crea el proyecto

---
+++ cmake-build-debug/CMakeFiles/arbol_binario_busqueda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arbol_binario_busqueda.dir/main.cpp.o"
+  "arbol_binario_busqueda"
+  "arbol_binario_busqueda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arbol_binario_busqueda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/arbol_binario_busqueda.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for arbol_binario_busqueda.
+# This may be replaced when dependencies are built.
